b829aaa629fd439005c4766869944ca0e970f3a3
johnervine@hotmail.com <johnervine@hotmail.com>
2015-11-18 13:50:28 +0000

    Fixed bug with transducer logic for handling followers

--- src/com/pav/profile/timeline/worker/functions/functions.clj
+++ src/com/pav/profile/timeline/worker/functions/functions.clj
@@ -10,7 +10,6 @@
   (assoc event :bill_title (retrieve-bill-title es-conn index (:bill_id event))))
 
 (defn retrieve-users-first-last-names [conn table-name user_id]
-  (println (far/get-item conn table-name {:user_id user_id} {:attrs [:first_name :last_name]}))
   (far/get-item conn table-name {:user_id user_id} {:attrs [:first_name :last_name]}))
 
 (defn event-transducer [es-conn index dy-conn table-name]
@@ -18,10 +17,10 @@
          "vote"           (assoc % :new-msg (add-bill-title es-conn index (:decoded-msg %)))
          "comment"        (-> (assoc % :new-msg (add-bill-title es-conn index (:decoded-msg %)))
                               (update-in [:new-msg] (fn [v] (assoc v :score 0))))
-         "followinguser"  (-> (retrieve-users-first-last-names dy-conn table-name (:following_id (:decoded-msg %)))
-                              (merge (:decoded-msg %))
-                              (assoc % :new-msg))
-         "followeduser"  (-> (retrieve-users-first-last-names dy-conn table-name (:follower_id (:decoded-msg %)))
-                              (merge (:decoded-msg %))
-                              (assoc % :new-msg))
+         "followinguser"  (->> (retrieve-users-first-last-names dy-conn table-name (:following_id (:decoded-msg %)))
+                               (merge (:decoded-msg %))
+                               (assoc % :new-msg))
+         "followeduser"   (->> (retrieve-users-first-last-names dy-conn table-name (:follower_id (:decoded-msg %)))
+                               (merge (:decoded-msg %))
+                               (assoc % :new-msg))
          nil)))
