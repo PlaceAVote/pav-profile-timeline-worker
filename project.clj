7492d26f059b9297c857ef727a8ab3328082b652
johnervine@hotmail.com <johnervine@hotmail.com>
2015-12-15 20:55:07 +0000

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav_profile_timeline_worker "0.1.0"
+(defproject pav_profile_timeline_worker "0.1.1-SNAPSHOT"
   :description "Process user activities from Redis and update the users timeline."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
